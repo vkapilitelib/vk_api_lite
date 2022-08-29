cec5d8c9c73a6a3fe39efa7cc31a1e49d1f729eb
Chesnokov MA <77567677+Chesnokov-ma@users.noreply.github.com>
2022-08-29 10:19:28 +1000

    .gitignore fix5

--- vk_api_lite/test.py
+++
@@ -1,5 +0,0 @@
-from vk_api_lite import VkAPILite
-
-vk_api_l = VkAPILite(TOKEN='vk1.a.OVgI7F9psMHdS4954SQ_6ksmDM0cFsZ6rCGYxbXJyR00A3Hqa76H6VI0anrkYLBTuxVtrXXT7fAK-YzSC78OhT2V_3FoqMQCwFHCi6utgVytIinZWNrxUlwKx_jPF7P0kgGbUq9QViyHvR0TUN35oobk7dBo8-QsiGT1afvS3KkKf0cbbgZ8UeAEiobq3Ctr')
-
-print(vk_api_l.wallGetById(215436856))
